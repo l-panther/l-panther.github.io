912f51a15832e19e001d7746f44da4d78f2dc736
l-panther <51133566+l-panther@users.noreply.github.com>
2021-03-26 06:20:43 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,28 @@
+Title
+-----------
+Portfolio: A website
+
+Author & Contact information
+-----------
+Leroy Russell 
+
+leroy_rus@hotmail.com
+
+Description
+-----------
+This website provides information about my front-end and back-end web development capabilities and highlights my current skills.
+
+Installation
+------------
+No installation needed.
+
+This website can be viewed by double clicking: index.html
+or by uploading files to a web server and navigating to the root web folder.
+
+Configuration
+-------------
+There is no configuration needed to run this website.
+
+Notes
+-----
+This project is an example of my front-end and back-end web development capabilities.
---
+++ js/custom.js
@@ -0,0 +1,102 @@
+(function($) {
+    "use strict";
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(50).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+    	
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
+	
+	$('.navbar-nav .nav-link').on('click', function() {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+	
+	$("#primarynav .open").click(function() {
+		$(this).hide();
+		$("#primarynav .close").show().click(function() {
+			$(this).hide();
+			$("#primarynav .open").show();
+		});
+		
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+
+	/* ..............................................
+    Scroll Element Animation 
+    ................................................. */
+	//setTimeout(startAnimationRight, 200);
+	//setTimeout(startAnimationLeft, 200);
+	
+	// alert($("#website-preview").scrollTop());
+		
+	/*
+	function startAnimationLeft() {
+		animationLeft("#mobile-desc");
+		animationLeft("#website-preview");
+	}	
+	
+	function startAnimationRight() {
+		animationRight("#mobile-preview");
+		animationRight("#website-desc");
+	}
+	
+	function animationRight(id) {
+		$(id).addClass("w3-animate-right");
+		$(id).fadeIn();
+	}
+	
+	function animationLeft(id) {
+		$(id).addClass("w3-animate-left");
+		$(id).fadeIn();
+	}
+	*/
+	
+	
+	/* ..............................................
+    Scroll To Top
+    ................................................. */
+	
+	$(document).ready(function () {
+
+		$(window).scroll(function () {
+			if ($(this).scrollTop() > 100) {
+				$('#scroll-to-top').fadeIn();
+			} else {
+				$('#scroll-to-top').fadeOut();
+			}
+		});
+
+		$('#scroll-to-top').click(function () {
+			$("html, body").animate({
+				scrollTop: 0
+			}, 600);
+			return false;
+		});
+
+	});
+	
+	
+
+	
+}(jQuery));
