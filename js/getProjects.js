b7c4aba668df4370d27b5061c0d10bc58c4fff41
l-panther <51133566+l-panther@users.noreply.github.com>
2021-05-03 01:50:44 +0100

    Add files via upload

--- js/getProjects.js
+++ js/getProjects.js
@@ -1,20 +1,25 @@
 $(document).ready(function() {
 	
-	let output = "",
-		output2 = "",
-		dots = "<span class='w3-badge dots w3-border'></span>",
-		projects = [
-			
-		{
-			"id": 1,
-			"name": "Cafes in London ",
-			"type": "A Website",
-			"image": "cafe",
-			"features":[ 
+    /*
+        constructor for class: id, title, type, image, features, technologies, reports, description, git) 
+        
+    */
+    
+	let carouselContent = "",
+		slideCount = "",
+		report = "",
+        
+        // Project object
+		project1 = new Project(
+			1,
+			"Cafes in London ",
+			"A Website",
+			"cafe",
+            [ 
 				{"name":"Carousel"},
 				{"name":"Google Map"}
 			],
-			"technologies":[ 
+			[ 
 				{"name":"HTML5"},
 				{"name":"CSS3"},
 				{"name":"bootstrap"},
@@ -22,21 +27,29 @@ $(document).ready(function() {
 				{"name":"JSON"},
 				{"name":"Krita"}
 			],
-			"description":"A website guide for cafes.",
-			"url":"cafe",
-			"git":"cafes"
-		},	
-			
-		{
-			"id": 2,
-			"name": "Car Repair",
-			"type": "A Website",
-			"image": "carrepair",
-			"features":[ 
+            [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"},
+				{"name":"cafe6"}
+			],
+			"A website guide for cafes.",
+			"cafe",
+			"cafes"
+		),	
+        
+        project2 = new Project(
+			2,
+			"Car Repair",
+			"A Website",
+			"carrepair",
+			[ 
 				{"name":"Modal"},
 				{"name":"Slideshow"}
 			],
-			"technologies":[ 
+            [ 
 				{"name":"HTML5"},
 				{"name":"CSS3"},
 				{"name":"bootstrap"},
@@ -44,21 +57,28 @@ $(document).ready(function() {
 				{"name":"JSON"},
 				{"name":"Krita"}
 			],
-			"description":"A website for company services.",
-			"url":"car-repair",
-			"git":"car-repair"
-		},	
-			
-		{
-			"id": 3,
-			"name": "Diabetes",
-			"type": "A Web Application",
-			"image": "diabetes",
-			"features":[ 
+            [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"}
+			],
+            "A website for company services.",
+			"car-repair",
+            "car-repair"
+		),	
+		
+        project3 = new Project(
+			3,
+			"Diabetes",
+			"A Web Application",
+			"diabetes",
+			[ 
 				{"name":"BMI Test"},
 				{"name":"Contact form"}
 			],
-			"technologies":[ 
+			[ 
 				{"name":"PHP"},
 				{"name":"HTML5"},
 				{"name":"CSS3"},
@@ -66,21 +86,28 @@ $(document).ready(function() {
 				{"name":"JavaScript"},
 				{"name":"Krita"}
 			],
-			"description":"A web application for diabetes.",
-			"url":"diabetes",
-			"git":"diabetes"
-		},	
-			
-		{
-			"id": 4,
-			"name": "Efexacademy",
-			"type": "A Website",
-			"image": "efexacademy",
-			"features":[ 
+           [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"}
+			],
+			"A web application for diabetes.",
+			"diabetes",
+			"diabetes"
+		),	
+        
+        project4 = new Project(
+			4,
+			"Efexacademy",
+			"A Website",
+			"efexacademy",
+			[ 
 				{"name":"Carousel"},
 				{"name":"Google Map"}
 			],
-			"technologies":[ 
+			[ 
 				{"name":"PHP"},
 				{"name":"HTML5"},
 				{"name":"CSS3"},
@@ -89,21 +116,28 @@ $(document).ready(function() {
 				{"name":"Maya"},
 				{"name":"Krita"}
 			],
-			"description":"A website for company services.",
-			"url":"efexacademy",
-			"git":"efexacademy"
-		},
+            [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"}
+			],
+			"A website for company services.",
+			"efexacademy",
+			"efexacademy"
+		),
 		
-		{
-			"id": 5,
-			"name": "Intranet",
-			"type": "A Website Application",
-			"image": "intranet",
-			"features":[ 
+		project5 = new Project(
+			5,
+			"Intranet",
+			"A Website Application",
+			"intranet",
+			[ 
 				{"name":"Login/Register"},
 				{"name":"Sessions"}
 			],
-			"technologies":[ 
+			[ 
 				{"name":"PHP"},
 				{"name":"HTML5"},
 				{"name":"CSS3"},
@@ -111,22 +145,28 @@ $(document).ready(function() {
 				{"name":"JavaScript"},
 				{"name":"Krita"}
 			],
-			"description":"A intranet for company services.",
-			"url":"intranet",
-			"git":"intranet"
-		},
+            [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"}
+			],
+			"A intranet for company services.",
+            "intranet",
+			"intranet"
+		),
 				
-			
-		{
-			"id": 6,
-			"name": "London Guide",
-			"type": "A Website",
-			"image": "london",
-			"features":[ 
+		project6 = new Project(	
+            6,
+			"London Guide",
+			"A Website",
+			"london",
+			[ 
 				{"name":"Modal"},
 				{"name":"Google Map"}
 			],
-			"technologies":[ 
+			[ 
 				{"name":"PHP"},
 				{"name":"HTML5"},
 				{"name":"CSS3"},
@@ -135,10 +175,17 @@ $(document).ready(function() {
 				{"name":"Krita"}
 
 			],
-			"description":"A website guide for London.",
-			"url":"london/index.php",
-			"git":"london-guide"
-		},
+            [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"}
+			],
+			"A website guide for London.",
+			"london/index.php",
+			"london-guide"
+		),
 		
 		/*{
 			"id": 7,
@@ -165,16 +212,16 @@ $(document).ready(function() {
 		},
 		*/
 			
-		{
-			"id": 7,
-			"name": "Parees Taylor",
-			"type": "A Website",
-			"image": "parees",
-			"features":[ 
+		project7 = new Project(
+			7,
+			"Parees Taylor",
+			"A Website",
+			"parees",
+			[ 
 				{"name":"Carousel"},
 				{"name":"Device Integration"}
 			],
-			"technologies":[ 
+			[ 
 				{"name":"HTML5"},
 				{"name":"CSS3"},
 				{"name":"bootstrap"},
@@ -182,10 +229,17 @@ $(document).ready(function() {
 				{"name":"Krita"},
 
 			],
-			"description":"A website for company services.",
-			"url":"parees/index.html",
-			"git":"parees"
-		}
+            [ 
+				{"name":"cafe1"},
+				{"name":"cafe2"},
+				{"name":"cafe3"},
+				{"name":"cafe4"},
+				{"name":"cafe5"}
+			],
+			"A website for company services.",
+			"parees",
+			"parees"
+		),
 			
 			
 		/*{
@@ -211,98 +265,22 @@ $(document).ready(function() {
 			"url":"housing",
 			"git":"housing"
 		}*/
-			
-	];
-	
+		
+        // Array of projects
+        projects = [project1, project2, project3, project4, project5, project6, project7];
 	
 	// For length of result, output content
 	for (var i = 0; i < projects.length; i++) {
-
-		if(projects[i].id == 1) { 
-			output += "<div class='carousel-item active'> <!-- Carousel item -->"; 
-			output2 += "<li data-target='#projectCarousel' data-slide-to='" + i + "' class='active'>" + i + "</li>"; 
-		} else if (projects[i].id != 1) {
-			output += "<div class='carousel-item'> <!-- Carousel item -->";
-			output2 += "<li data-target='#projectCarousel' data-slide-to='" + i + "'></li>"; 
-		}
-
-		// Output
-		output += "<h2 class='theme-color-bg text-center'>" + projects[i].name + "</h2>";
-		output += "<div class='row w3-container'> <!-- Row -->";
-		output += "<div class='col-lg-5 col-md-5 col-sm-12 w3-center'> <!-- Column -->";
-		output += "<img class='img-carousel' src='images/projects/" + projects[i].image + ".png' alt='Website " + projects[i].id + " screenshot' />";
-		output += "</div> <!-- End column-->";
-		output += "<div class='col-lg-7 col-md-7 col-sm-12'> <!-- Column -->";
-		
-		// Description
-		output += "<h3><b>Description</b></h3>";
-		output += "<div class='project-desc'>";
-		output += "<p>" + projects[i].description + "</p>";
-		output += "</div>";
-		
-		// Features
-		output += "<h3 class='w3-margin-top'><b>Features</b></h3>";
-		output += "<ul>";
-		
-		for (let j = 0; j < projects[i].features.length; j++) {
-			output += "<li><i class='fa fa-check w3-margin-right'></i>" + projects[i].features[j].name + "</li>";
-		
-		}
-		output += "</ul>";
-		
-		// Technologies
-		output += "<h3 class='w3-margin-top'><b>Technologies</b></h3>";
-		output += "<ul class='tech-list'>";
-		
-		for (let k = 0; k < projects[i].technologies.length; k++) {
-			output += "<li><img src='images/technologies/" + projects[i].technologies[k].name + ".png' alt='' title='" + projects[i].technologies[k].name + "' class='tech-icon'></li>";
-		
-		}
-		output += "</ul>";
-		output += "</div> <!-- End column -->";
-		/*
-		output += "<div class='col-lg-12'> <!-- Column -->";
-		output += "<h3 class='w3-margin-top'>Features</h3>";
-		output += "<ul class='w3-text-grey'>";
-		
-		for (let k = 0; k < projects[i].features.length; k++) {
-			output += "<li><i class='fa fa-check w3-margin-right'></i>" + projects[i].features[k].name + "</li>";
-		
-		}
-		output += "</ul>";
-		output += "</div> <!-- End column -->";
-		*/
-	
-		output += "</div> <!-- End row -->";
-		
-		/* Mockup button
-		output += "<div class='w3-row w3-center buttons'> <!-- Row -->";
-		output += "<div class='w3-col s4 m4 l4 button'> <!-- Column -->";
-		output += "<div class='w3-padding' title='View website in new tab'><i class='fa fa-globe'></i><a href='data/projects/" + projects[i].url + "' target='_blank'><span class='w3-hide-small'>VIEW</span> WEBSITE</a></div>";
-		output += "</div> <!-- End column -->";
-		*/
-		
-		// Website button
-		output += "<div class='w3-row w3-center buttons'> <!-- Row -->";
-		
-		output += "<div class='w3-col s6 m6 l6 button'> <!-- Column -->";
-		output += "<div class='w3-padding' title='View website in new tab'><i class='fa fa-globe'></i><a href='https://l-panther.github.io/data/projects/" + projects[i].url + "' target='_blank'><span class='w3-hide-small'>VIEW</span> WEBSITE</a></div>";
-		output += "</div> <!-- End column -->";		
-		
-		
-		// Code button
-		output += "<div class='w3-col s6 m6 l6 button'> <!-- Column -->";
-		output += "<div class='w3-padding' title='View source code in Github'><i class='fa fa-list'></i><a href='https://github.com/l-panther/" + projects[i].git + "' target='_blank'><span class='w3-hide-small'>VIEW</span> CODE</a></div>";
-		output += "</div> <!-- End column -->";
-		output += "</div <!-- End row -->";
-		output += "</div <!-- End row -->";
-		
+        carouselContent += projects[i].showCarousel(i);
+        
 	}
-	               
-	// $("#website-dots").html(dots.repeat(projects.length)); // Insert content to element and refresh listview
-	$("#projectList").html(output); // Insert output into html 
-	$("#slideCount").html(output2); // Insert output into html 
-	
-	
+    for (var j = 0; j < projects.length; j++) {
+        projects[j].showReport();
+	}
+     
+    // Insert output into html 
+	$("#projectList").html(carouselContent); 
+	$("#slideCount").html(slideCount); 
 });
+
 
