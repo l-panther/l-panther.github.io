6bad38f821ebf7c721e5d0c2a07c48765c38673e
Leroy Russell <51133566+l-panther@users.noreply.github.com>
2022-09-26 02:17:38 +0100

    Add files via upload

--- js/classes.js
+++ js/classes.js
@@ -164,17 +164,10 @@ class Skillset extends Developer {
 				if(this.id === 4) {
 					output += `
 						<div class="row video-container">
-							<div class="col-sm-6">
+							<div class="col-sm-12">
 								<video controls poster="">
 									<source src="media/mould_before.mp4" type="video/mp4">
 									<source src="media/mould_before.webm" type="video/webm">
-									Your browser does not support the video tag.
-								</video>
-							</div>
-							<div class="col-sm-6">
-								<video controls poster="">
-									<source src="media/mould_after.mp4" type="video/mp4">
-									<source src="media/mould_after.webm" type="video/webm">
 									Your browser does not support the video tag.
 								</video>
 							</div>
