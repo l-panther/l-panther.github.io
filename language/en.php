b91f979dc6b6a234a4a2dd433d34ebb3866c1b97
Leroy <51133566+l-panther@users.noreply.github.com>
2024-05-26 18:45:42 +0100

    Add files via upload
    Added language folder

---
+++ language/en.php
@@ -0,0 +1,12 @@
+<?php
+// Message if connection to DB fails
+$lang['db_error'] = 'Sorry, there has been an error. Please
+try again later.';
+
+// Message if query error
+$lang['mysqli_error'] = 'Sorry, there has been an error. Please try again later.';
+
+// Message if query returns no matching records
+$lang['empty_result_error'] = 'Sorry, there are no records to display.';
+
+?>
